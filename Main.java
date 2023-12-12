0423b6868820eedeb8635635edbea8dd8b14911e
IGNN3LZ3R0 <128260603+IGNN3LZ3R0@users.noreply.github.com>
2023-12-12 03:09:02 +0000

    he realizado la clase madre llamada figurageometrica junto con cada atributo de esta y sus metodos, ademas de la subclase poligono con sus subatributos y sus submetodos

--- Main.java
+++ Main.java
@@ -1,10 +1,43 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Testeo");
-    int x=1 ;
-    int y=2;
-    int z = 3;
-    int w=4;
-    float a=1.1;
-  }
+class FiguraGeometrica {
+    public int numeroLados;
+    public double area;
+    public double perimetro;
+
+    public FiguraGeometrica(int numeroLados) {
+        this.numeroLados = numeroLados;
+        this.area = 0.0;
+        this.perimetro = 0.0;
+    }
+    public void calcularArea() {
+    }
+    public void calcularPerimetro() {
+    }
+    public int obtenerNumeroLados() {
+        return numeroLados;
+    }
+    public double obtenerArea() {
+        return area;
+    }
+    public double obtenerPerimetro() {
+        return perimetro;
+    }
+}
+class Poligono extends FiguraGeometrica {
+    private double longitudLado;
+    public Poligono(int numeroLados, double longitudLado) {
+        super(numeroLados);
+        this.longitudLado = longitudLado;
+    }
+    public double obtenerLongitudLado() {
+        return longitudLado;
+    }
+    @Override
+    public void calcularArea() {
+        this.obtenerNumeroLados(); 
+        this.area = 0.25 * obtenerNumeroLados() * Math.pow(longitudLado, 2) / Math.tan(Math.PI / obtenerNumeroLados());
+    }
+    @Override
+    public void calcularPerimetro() {
+        this.perimetro = obtenerNumeroLados() * longitudLado;
+    }
 }
